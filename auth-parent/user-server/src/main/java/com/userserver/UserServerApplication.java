58adb690ebd5a4ed8992727f43c21f0ab26ede50
wxy <2363828884@qq.com>
2021-03-23 11:18:25 +0800

    提交代码

---
+++ auth-parent/user-server/src/main/java/com/userserver/UserServerApplication.java
@@ -0,0 +1,17 @@
+package com.userserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 资源服务
+ * @author 23638
+ */
+@SpringBootApplication
+public class UserServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServerApplication.class, args);
+    }
+
+}
